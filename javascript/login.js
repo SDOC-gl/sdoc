99cdc43b431a07009d6743f65227351a8bd54c9b
Sunlol <pedrohenriquesilvapaulo5@gmail.com>
2024-08-14 17:08:29 -0300

    Login update

--- javascript/login.js
+++ javascript/login.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (user) {
        // sendWebhook("SITE - LOGIN", `O player: \`${user}\`\n**Entrou no site**.`);
-        window.location.href = 'site.html';
+        window.location.href = './site/';
     }
 
 });
