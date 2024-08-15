5648552127307dc42441ff9ceab0cf23a288e75a
Sunlol <pedrohenriquesilvapaulo5@gmail.com>
2024-08-15 15:42:58 -0300

    Webhook updateeee

--- dataModule.js
+++ dataModule.js
@@ -1,11 +1,20 @@
-// Função para enviar o webhook.
-function sendWebhook(titulo, descricao) {
+// Função para enviar o webhook com diferentes tipos de notificações
+function sendWebhook(titulo, descricao, tipo = 'info') {
     const webhookUrl = "https://discord.com/api/webhooks/1273020022800781494/Y8Ib_CCKtaui7yNP6DZysUSlbx1xxZySc6g5FlhwiOUIOTJmzTjbiV8VICC75911gaS_";
+
+    // Definindo cores para diferentes tipos de notificações
+    const cores = {
+        info: 3447003,     // Azul
+        warning: 15105570, // Amarelo
+        error: 15158332,   // Vermelho
+        success: 3066993   // Verde
+    };
 
     const data = {
         embeds: [{
             title: titulo,
             description: descricao,
+            color: cores[tipo] || cores.info,  // Usa a cor correspondente ao tipo ou a cor de info como padrão
             timestamp: new Date().toISOString(),
         }],
     };
@@ -18,9 +27,9 @@ function sendWebhook(titulo, descricao) {
         },
         body: JSON.stringify(data)
     })
-        .then(response => response.text())
-        .then(text => console.log(text))
-        .catch(error => console.error('Erro:', error));
+    .then(response => response.text())
+    .then(text => console.log(text))
+    .catch(error => console.error('Erro:', error));
 }
 
 // Função para obter valor do cookie
