9f98cbafde1bf398f9cb7d06e6e31d81f7ef2e96
Atliylol <hollowfansparteii@gmail.com>
2024-08-18 13:06:44 -0300

    local json fetch & JQUERY

--- site.js
+++ site.js
@@ -106,79 +106,26 @@ function process(display) {
 
 
     const user = data.getCookie('user');
-    const jsonUrl = 'https://raw.githubusercontent.com/Atliylol/atliylol.github.io/main/resources/inputs.json';
 
-    fetch(jsonUrl)
-        .then(response => response.json())
-        .then(processings => {
-            let input_value = (String)(display.toUpperCase());
+    $.getJSON("../resources/inputs.json", function(json) {
+        let input_value = (String)(display.toUpperCase());
+        let hasInput = false;
+        $.each(json, function (i, value) { 
+            
+            if (value.input.includes(input_value)) {
+                hasInput = true;
+                const isRandom = (Boolean)(value.random);
 
-            for (const processing of processings) {
-                if (processing.input.map(item => item.toUpperCase()).includes(input_value)) {
+                let content = value.result;
 
-                    const isRandom = (Boolean)(processing.random);
+                if (typeof content == "object" && isRandom) {
+                    content = content[Math.round(Math.random())];
+                }
 
-                    let content = processing.result;
+                content.trim();
+                input_value.trim()
 
-                    if (typeof content == "object" && isRandom) {
-                        content = content[Math.round(Math.random())];
-                    }
-
-                    content.trim();
-                    input_value.trim()
-
-                    if (/^https?:\/\//.test(content)) { // Verifica se é um link //
-
-                        data.sendWebhook(
-                            "SITE - LOG-BUSCA", 
-                            `O player: \`${user}\`\n\nBuscou com o parâmetro: \`${display}\`\nResultado: \`${content}\``,
-                            'success',
-                        );
-                        
-                        window.open(content, '_blank');
-                        
-                        return;
-                    }
-
-                    if (/\.(pdf|jpeg|jpg|png|webm|webp)$/i.test(content)) { // Verifica se é uma foto/video //
-
-                        data.sendWebhook(
-                            "SITE - LOG-BUSCA",
-                            `O player: \`${user}\`\n\nBuscou com o parâmetro: \`${display}\`\nResultado: \`${content}\``,
-                            'success',
-                        );
-                        openMedia("../resources/" + content);                        
-                        return;
-                    }
-
-                    if (content === "EXIT()") { // Faz o logout //
-
-                        let username = data.getCookie('user');
-
-                        data.removeCookie('user');
-
-                        // João queiroz
-
-
-                        data.removeCookie('last_webhook_time');
-
-
-                        if (!data.getCookie('user') || !data.getCookie('last_webhook_time')) {
-
-                            data.sendWebhook(
-                                "SITE - SAIU",
-                                `O player: \`${username}\`\n\n**Saiu do site usando EXIT() !**`,
-                                'warning'
-                            )
-
-                            window.location.href = '../index.html';
-                        } else {
-                            console.log("Houve um erro");
-                        }
-                        return;
-                    }
-
-                    document.getElementById('displayText').innerHTML = content;
+                if (/^https?:\/\//.test(content)) { // Verifica se é um link //
 
                     data.sendWebhook(
                         "SITE - LOG-BUSCA", 
@@ -186,15 +133,58 @@ function process(display) {
                         'success',
                     );
                     
+                    window.open(content, '_blank');
+                    
                     return;
                 }
+
+                if (/\.(pdf|jpeg|jpg|png|webm|webp)$/i.test(content)) { // Verifica se é uma foto/video //
+
+                    data.sendWebhook(
+                        "SITE - LOG-BUSCA",
+                        `O player: \`${user}\`\n\nBuscou com o parâmetro: \`${display}\`\nResultado: \`${content}\``,
+                        'success',
+                    );
+                    openMedia("../resources/" + content);                        
+                    return;
+                }
+
+                if (content === "EXIT()") { // Faz o logout //
+
+                    let username = data.getCookie('user');
+
+                    data.removeCookie('user');
+                    data.removeCookie('last_webhook_time');
+
+                    if (!data.getCookie('user') || !data.getCookie('last_webhook_time')) {
+
+                        data.sendWebhook(
+                            "SITE - SAIU",
+                            `O player: \`${username}\`\n\n**Saiu do site usando EXIT() !**`,
+                            'warning'
+                        )
+
+                        window.location.href = '../index.html';
+                    } else {
+                        console.log("Houve um erro");
+                    }
+                    return;
+                }
+
+                document.getElementById('displayText').innerHTML = content;
+
+                data.sendWebhook(
+                    "SITE - LOG-BUSCA", 
+                    `O player: \`${user}\`\n\nBuscou com o parâmetro: \`${display}\`\nResultado: \`${content}\``,
+                    'success',
+                );
+                return;
             }
-
-
-
-            document.getElementById('displayText').textContent = "Tente novamente.";
-        })
-        .catch(error => console.error('Erro ao processar JSON:', error));
+            if (!hasInput)
+                document.getElementById('displayText').innerHTML = "Entrada não encontrada<br>Tente novamente.";
+            
+        });
+    });
 }
 
 // Manipular o envio do formulário
