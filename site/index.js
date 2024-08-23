bc5b962facd6ddc44515e2a271a99756aafcba4a
Atliylol <hollowfansparteii@gmail.com>
2024-08-23 19:37:23 -0300

    AX2OM!!!

--- site/index.js
+++ site/index.js
@@ -1,7 +1,7 @@
 // Libs 
 
-import data from './modules/dataModule.js';
-import tableObjects, { TableObject } from './modules/tableObjects.js'
+import data from '../modules/dataModule.js';
+import tableObjects, { TableObject } from '../modules/tableObjects.js'
 
 function print(text = Number) {
     console.log(text);
@@ -134,9 +134,9 @@ function lightCandle() {
     const candleLight = document.getElementById("candle-light");
     candleLight.style.visibility = "visible"
     const candle = document.getElementById("candle");
-    candle.src = "../IMG/candle-on.gif";
+    candle.src = "./IMG/candle-on.gif";
     const table = document.getElementById("table");
-    table.src = "../IMG/table-2.png";
+    table.src = "./IMG/table-2.png";
     const mysteryWall = document.getElementById("mystery-wall");
     mysteryWall.style.visibility = "visible";
 }
@@ -144,11 +144,33 @@ function lightCandle() {
 function whenDie() {
     document.getElementById('displayText').innerHTML = data.getCookie('deathseed');
 }
+function openGBScreen() {
+    setTimeout(() => {
+        document.getElementById('displayText').innerHTML = "";
+    }, 2500);
+    const html = document.body;
+    html.animate(
+        [
+            // keyframes
+            { transform: "scale(1, 1) translateX(0px) translateY(0px)", opacity: 1 },
+            { transform: "scale(4.5, 4.5) translateX(220px) translateY(100px)", opacity: 0 },
+        ],
+        {
+            // timing options
+            duration: 3500,
+            easing: "cubic-bezier(1,.02,.84,.95)"
+        },
+    ).onfinish = function () {
+        setTimeout(function() {
+            window.open("../AX2OM", "_self");
+        }, 500)
+    };
+    html.style.opacity = 0;
+}
 
 
 function process(display) {
     if (!canDoAnything) return;
-
 
     const user = data.getCookie('user');
 
@@ -178,6 +200,11 @@ function process(display) {
 
             if (typeof content == "object" && isRandom) {
                 content = content[Math.round(Math.random())];
+            }
+
+            if (input_value == "AX2OM_NRGT") {
+                openGBScreen();
+                return;
             }
 
             if (content.startsWith("function=")) {
